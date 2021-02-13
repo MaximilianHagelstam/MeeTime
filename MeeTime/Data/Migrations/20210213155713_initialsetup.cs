0bc9540bb2aee5cbc59b89df3e7dd749cd50c83c
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-02-13 18:12:34 +0200

    Add db connection

---
+++ MeeTime/Data/Migrations/20210213155713_initialsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeeTime.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MeetName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MeetCode = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meet", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Meet");
+        }
+    }
+}
