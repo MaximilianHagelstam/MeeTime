5a6fdedd985fbaf57d9376876b3de2080bb01a53
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-02-13 13:29:58 +0200

    Create meet model

---
+++ MeeTime/Controllers/Meet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeeTime.Controllers
+{
+    public class Meet
+    {
+        public int Id { get; set; }
+
+        public string MeetName { get; set; }
+
+        public string MeetCode { get; set; }
+
+        public Meet()
+        {
+
+        }
+
+
+    }
+}
